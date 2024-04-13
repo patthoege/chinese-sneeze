1fe851a5e099619a8944d4925ee83cf134384c6f
patthoege <paty.hoge@gmail.com>
2024-04-13 15:40:39 +0200

    Optimize categories container in App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'T-Shirts',
+    },
+    {
+      id: 2,
+      title: 'Hoodies',
+    },
+    {
+      id: 3,
+      title: 'Bags',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
+
   return (
-    <div className="categories-container">
-      <div className="category-container">
-         {/* <img /> */}
-         <div className="category-body-container">
-          <h2>Womens</h2>
+    <div className='categories-container'>
+    {categories.map(({title}) => (
+      <div className='category-container'>
+         <div className='background-image' />
+         <div className='category-body-container'>
+          <h2>{title}</h2>
           <p>Shop Now</p>
          </div>
       </div>
-      <div className="category-container">
-         {/* <img /> */}
-         <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-         </div>
-      </div>
-      <div className="category-container">
-         {/* <img /> */}
-         <div className="category-body-container">
-          <h2>T-Shirts</h2>
-          <p>Shop Now</p>
-         </div>
-      </div>
-      <div className="category-container">
-         {/* <img /> */}
-         <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-         </div>
-      </div>
-      <div className="category-container">
-         {/* <img /> */}
-         <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-         </div>
-      </div>
+    ))}
     </div>
   );
 };
