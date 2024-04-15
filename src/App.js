ee8c14ed7cc228c95b1806460161ab75bafcca59
patthoege <paty.hoge@gmail.com>
2024-04-15 13:11:23 +0200

    Created menu component/styles files and import into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Menu from "./components/menu/menu.component";
 
 const App = () => {
 
@@ -32,11 +31,7 @@ const App = () => {
   ];
 
   return (
-    <div className='categories-container'>
-    {categories.map((category) => (
-      <CategoryItem key={category.id} category={category} />
-    ))}
-    </div>
+    <Menu categories={categories} />
   );
 };
 
