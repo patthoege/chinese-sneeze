a058f349b9f36cf43eb475ba71ed9f1ea0d8be78
patthoege <paty.hoge@gmail.com>
2024-04-15 18:18:48 +0200

    Add NavBar and import Outlet to leverage the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+          Navbar
+        </h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<NavBar />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
