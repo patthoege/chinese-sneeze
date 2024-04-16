e1e6a4ee3790a01c190ba7cb5105213c71be9863
patthoege <paty.hoge@gmail.com>
2024-04-16 13:42:44 +0200

    Add defaultFormFields object and set handleChange function to sign up form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,19 +1,65 @@
+import { useState } from "react";
+
+
+const defaultFormFields = {
+    name: '',
+    email:'',
+    password:'',
+    confirmPassword:''
+}
+
 const SignUpForm = () => {
+
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { name, email, password, confirmPassword } = formFields;
+
+    console.log(formFields);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormFields({ ...formFields, [name]: value });
+    };
+
     return (
         <div>
             <h1>Sign up</h1>
             <form onSubmit={() =>{}}>
                 <label>Name</label>
-                <input type="text" required/>
+                <input 
+                    onChange={handleChange}
+                    type="text"
+                    name="name"
+                    value={name}
+                    required
+                />
 
                 <label>Email</label>
-                <input type="email" required/>
+                <input 
+                    onChange={handleChange} 
+                    type="email" 
+                    name="email"
+                    value={email}
+                    required
+                />
 
                 <label>Password</label>
-                <input type="password" required/>
+                <input 
+                    onChange={handleChange}
+                    type="password" 
+                    name="pasword"
+                    value={password}
+                    required
+                />
 
                 <label>Confirm Password</label>
-                <input type="password" required/>
+                <input 
+                    onChange={handleChange} 
+                    type="password" 
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    required
+                />
 
                 <button type="submit">Sign up</button>
             </form>
