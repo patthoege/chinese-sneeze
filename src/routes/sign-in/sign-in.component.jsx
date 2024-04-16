f172ff4922fe47ddddbbb8d9ec793146bff8b6f8
patthoege <paty.hoge@gmail.com>
2024-04-16 13:14:11 +0200

    Add signInWithGoogleRedirect and change to googleProvider

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,20 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
     const logGoogleUser = async() => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
+    };
 
     return (
       <div>
-      <h1>SignIn Page</h1>
-      <button onClick={logGoogleUser}>
-      Sign in with google popup
-      </button>
+        <h1>SignIn Page</h1>
+        <button onClick={logGoogleUser}>
+            Google Popup
+        </button>
       </div>  
     )
 }
