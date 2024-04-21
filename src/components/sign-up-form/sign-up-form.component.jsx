5709b88740d7cc838fc8d5294363ed4a53838b67
patthoege <paty.hoge@gmail.com>
2024-04-21 12:44:24 +0200

    Import UserContext and set current user value into sign up component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -9,6 +9,7 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import './sign-up-form.styles.scss';
+import { UserContext } from "../../contexts/user.context";
 
 const defaultFormFields = {
     name: '',
@@ -22,7 +23,7 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { name, email, password, confirmPassword } = formFields;
 
-    console.log(formFields);
+    const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -41,6 +42,8 @@ const SignUpForm = () => {
                 email, 
                 password
             );
+
+            setCurrentUser(user);
 
             await createUserDocumentFromAuth(user, { name });
             resetFormFields();
