847088c422c91b3ebedcbfa1e5b7ffd197c9c7f4
patthoege <paty.hoge@gmail.com>
2024-04-24 20:17:50 +0200

    Create cart-dropdown component and styles and import into navbar

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { UserContext } from '../../contexts/user.context';
 
@@ -34,6 +35,7 @@ const NavBar = () => {
                 )}
                 <CartIcon />
             </div>
+            <CartDropdown />
         </div>
         <Outlet />
       </Fragment>
