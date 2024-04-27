0cb1300e835d3265e635bf5050ef56f6ff7d6f70
patthoege <paty.hoge@gmail.com>
2024-04-27 13:19:32 +0200

    added increment addItemToCart functionality into checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 const Checkout = () => {
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, addItemToCart } = useContext(CartContext);
     return(
         <div>
             <h1>Checkout page</h1>
@@ -14,7 +14,9 @@ const Checkout = () => {
                     <div key={id}>
                         <img src={imageUrl} alt={`${name}`} />
                         <h2>{name}</h2>
+                        <span>-</span>
                         <span>{quantity}</span>
+                        <span onClick={() => addItemToCart(cartItem)}>+</span>
                     </div>
                 )
               })
