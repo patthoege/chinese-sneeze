558992820434f80c92814303562466860e04a489
patthoege <paty.hoge@gmail.com>
2024-04-27 13:03:34 +0200

    added map method over cartItem and set return cart items into  checkout component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,25 @@
+import { useContext } from 'react';
 
+import { CartContext } from '../../contexts/cart.context';
 
 const Checkout = () => {
+    const { cartItems } = useContext(CartContext);
     return(
         <div>
-        <h1>Checkout page</h1>
+            <h1>Checkout page</h1>
+            <div>
+            {cartItems.map((cartItem) => {
+                const {id, imageUrl, name, quantity} = cartItem;
+                return (
+                    <div key={id}>
+                        <img src={imageUrl} alt={`${name}`} />
+                        <h2>{name}</h2>
+                        <span>{quantity}</span>
+                    </div>
+                )
+              })
+            }
+            </div>
         </div>
     )
 }
