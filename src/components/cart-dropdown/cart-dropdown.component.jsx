262f37cd2433980eb8f939e2a9f20989f3dc0738
patthoege <paty.hoge@gmail.com>
2024-04-27 12:50:07 +0200

    added goToCheckoutHandler navigate function in cart-dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { CartContext } from '../../contexts/cart.context'; 
 
 import Button from '../button/button.component';
@@ -9,6 +11,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
 
     return (
         <div className='cart-dropdown-container'>
@@ -17,7 +24,7 @@ const CartDropdown = () => {
                     <CartItem key={item.id} cartItem={item} />
                 ))}
             </div>
-            <Button>checkout</Button>
+            <Button onClick={goToCheckoutHandler}>checkout</Button>
         </div>
     )
 };
