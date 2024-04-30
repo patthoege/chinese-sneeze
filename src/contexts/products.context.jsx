a13b0adf63d81ec5f40e1aa2b551ba6ce6d48b0e
patthoege <paty.hoge@gmail.com>
2024-04-30 16:11:35 +0200

    updated shop data js file, created addCollecttionAndDocuments method and import into products context to upload our shop data to firestore db

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,10 +1,20 @@
-import { createContext, useState } from 'react';
-import PRODUCTS from '../shop-data.json';
+import { createContext, useEffect, useState } from 'react';
+
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+
+// import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({ products: []});
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+
+    // delete this use effect, because every time that it runs, 
+    // it's going to try and set new values inside of the database
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, [])
+
     const value = {products};
     return(
         <ProductsContext.Provider value={value}> {children} </ProductsContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -14,7 +14,9 @@ import {
     getFirestore,
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -40,6 +42,19 @@ const firebaseConfig = {
   export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done');
+  };
 
   export const createUserDocumentFromAuth = async (
     userAuth, 
@@ -88,4 +103,3 @@ const firebaseConfig = {
 
   export const onAuthStateChangedListener = (callback) => 
     onAuthStateChanged(auth, callback);
-  
