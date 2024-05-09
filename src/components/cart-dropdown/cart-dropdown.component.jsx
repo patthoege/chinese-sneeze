4167948cef21f22b440f100e51f6517726ea10ec
patthoege <paty.hoge@gmail.com>
2024-05-09 10:58:55 +0200

    added styled componenet to cart-dropdown comp styles

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,7 +7,11 @@ import Button from '../button/button.component';
 
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import {
+  CartDropDownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -18,14 +22,14 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
+        <CartDropDownContainer>
+            <CartItems>
                 {cartItems.map((item) => (
                     <CartItem key={item.id} cartItem={item} />
                 ))}
-            </div>
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>checkout</Button>
-        </div>
+        </CartDropDownContainer>
     )
 };
 
