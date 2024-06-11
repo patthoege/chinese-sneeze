158048a403325ae895b14c60d03689011184d6f6
Patricia Höge <paty.hoge98@gmail.com>
2024-06-11 20:03:30 +0200

    created middleware folder and pass the loggerMiddleware function and import into store and set filter env in production

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,9 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger";
+import { loggerMiddleware } from "./middleware/logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type) {
-        return next(action);
-    }
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state: ', store.getState());
-};
 
 const persistConfig = {
     key: 'root',
@@ -26,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV != 'production' && loggerMiddleware].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
